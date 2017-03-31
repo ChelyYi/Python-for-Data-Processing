e7cbe82606705b48ae837db5472b0374fd0f2997
ChelyYi <197163391@qq.com>
2017-03-31 11:30:25 +0800

    final version of all attributes extraction
    get all attributes from original data file, including title, abstract, conference, proceeding, index_terms, and all title terms, paper feature(use abstract and title)

--- get_attribute.py
+++ get_attribute.py
@@ -1,42 +1,40 @@
 # -*- coding: utf-8 -*-
 import json
 import nltk
+import re
+
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet
 
+
 """
+from pprint import pprint
 with open('key2_result', encoding = 'utf-8') as data_file:
     line = data_file.readline()
-        data = json.loads(line)
-        pprint(data)
-        print(data["abstract"])
-
-        for item in data["authors"]:
-            print(item, end=',')
-        #print(data["authors"])
-        print('\t')
-
-        for item in data["cite"]:
-            print(item,end=' ')
-        #print(data["cite"])
-        print('\t')
-
-        print(data["index_term"])
-
-        for item in data["institutes"]:
-            print(item, end=',')
-        #print(data["institutes"])
-        print('\t')
-
-        print(data["publish"])
-
-        for item in data["refs"]:
-            print(item, end='')
-        #print(data["refs"])
-        print('\t')
-
-        print(data["title"])
+    data = json.loads(line)
+    pprint(data)
+    print(data["abstract"])
+    for item in data["authors"]:
+        print(item, end=',')
+    #print(data["authors"])
+    print('\t')
+    for item in data["cite"]:
+        print(item,end=' ')
+    #print(data["cite"])
+    print('\t')
+    print(data["index_term"])
+    for item in data["institutes"]:
+        print(item, end=',')
+    #print(data["institutes"])
+    print('\t')
+    print(data["publish"])
+    for item in data["refs"]:
+        print(item, end='')
+    #print(data["refs"])
+    print('\t')
+    print(data["title"])
 """
+
 
 def get_paper(fileName):
     """Input is the file name.
@@ -67,7 +65,7 @@ def get_term(limit):
             title_list.append(title)
 
     title_text = ' '.join(title_list)
-    title_list = process_string(title_text)
+    title_list = process_string(title_text) # title terms list after processing
     fdist = nltk.FreqDist( nltk.Text(title_list))
 
     output = open('term.txt', 'wt', encoding='utf-8')
@@ -78,30 +76,32 @@ def get_term(limit):
         if tuple[1] < limit: # low-frequency words
             continue
 
-        output.write(str(count) + '\t' + tuple[0] +  '\n')
+        output.write(str(count) + '\t' + tuple[0] + '\t'+ str(tuple[1])+ '\n')
         count = count + 1
 
 
 def get_feature(fileName):
     """This function is used for generate a feture matrix of all samples.
     Output feature.txt, of which each line is a sample and each column is a word feature.
-    It calls get_abstract() for extracting abstracts from original file, and feature_word_frequency() for getting feature words list"""
+    It calls feature_word_frequency() for getting feature words list"""
 
     #get_abstract(fileName)
     feature = feature_word_frequency(20) # a word feature list sorted by frequency, word frequency > 20
     result_file = open('feature.txt', 'wt', encoding='utf-8')
-    with open("abstract.txt", encoding='utf-8') as data_file:
+    with open(fileName, encoding='utf-8') as data_file:
         count = 1;
         for line in data_file:
+            data = json.loads(line)
             print("SAMPLE ", count)
             count = count+1
-            item = line.strip().split('\t')
-            abstract = item.pop().strip('\n').lower()
-
-            if abstract == "an abstract is not available.":
-                words_list = []
+            title = data["title"].strip('\n').lower()
+            if "abstract" in data:
+                abstract = data["abstract"].replace('\n',' ').lower()
             else:
-                words_list = process_string(abstract)
+                abstract = "An abstract is not available."
+
+            text = title +' '+ abstract
+            words_list = process_string(text)
 
             # match sample words to feature word as well as count
             for f in feature:
@@ -138,17 +138,24 @@ def get_abstract(fileName):
 
 def feature_word_frequency(frequency):
     """Input is the feature words frequency, when a word's frequency larger than this value, it's chosen as feature.
-    This function is used for extracting feature from abstract of all samples.
+    This function is used for extracting feature from abstract and title of all samples.
     In the result file, each line represents a sample's abstract, and each column is a feature word.
     The value is word frequency
     Return feature list, sorted by frequency"""
 
     print("FUNCTION: feature_word_frequency: ", frequency)
     text = []
+    # abstract
     with open("abstract.txt", encoding = 'utf-8') as data_file:
         for line in data_file:
            item = line.strip().split('\t')
            text.append(item.pop(1).strip('\n').lower()) # all in lowcase
+
+    # title
+    with open("paper.txt",encoding='utf-8') as title_file:
+        for line in title_file:
+            item = line.strip().split('\t')
+            text.append(item.pop().strip().lower())
 
     text_string = ' '.join(text)
     #process text string
@@ -159,14 +166,14 @@ def feature_word_frequency(frequency):
     #print(fdist.most_common() )
 
     #write all feature words into a file
-    output = open('abstract_words_feature','wt',encoding='utf-8')
+    output = open('abstract_title_feature','wt',encoding='utf-8')
     count = 1
     feature = []
     for tuple in fdist.most_common():
-        print(tuple[0]," ",tuple[1])
         # only use high-frequency words as the feature
         if tuple[1] >= frequency:
             feature.append(tuple[0])
+            print(tuple[0], " ", tuple[1])
 
         output.write(str(count) + '\t' + tuple[0] +'\t'+ str(tuple[1]) +'\n')
         count = count + 1
@@ -179,13 +186,13 @@ def process_string(string):
     Input a string needing to be process,
     Return a word list of this string( Note: Contains repeating words!!!)"""
     # tokenize
-    print("Stsrt tokenzizing...")
+    print("Start tokenizing...")
     # split text into tokens and remove punctuation and numeric character
     tokens = nltk.RegexpTokenizer(r'[a-z]+').tokenize(string)
     print("Tokens length: ",len(tokens))
 
     # delete stop words
-    print("Stsrt deleting stop words...")
+    print("Start deleting stop words...")
     stopwords_list = build_stopwords_list()
     vocabulary = []
     for word in tokens:
@@ -195,18 +202,19 @@ def process_string(string):
     # print(vocabulary)
 
     # words tagging
-    print("Stsrt words tagging...")
+    print("Start words tagging...")
     vocabulary_tag = nltk.pos_tag(vocabulary)
     # print(vocabulary_tag)
 
     # lemmatization
-    print("Stsrt limmatizing...")
+    print("Start limmatizing...")
     lemmatizer = WordNetLemmatizer()
     words_list = []
     for tuple in vocabulary_tag:
         word = lemmatizer.lemmatize(tuple[0], pos=get_wordnet_pos(tuple[1]))  # get normal words
         # print(word)
-        words_list.append(word)
+        if word not in stopwords_list:
+            words_list.append(word)
     print("The number of words: ",len(set(words_list)))
     return words_list
 
@@ -263,6 +271,7 @@ def get_index_term(fileName):
             output.write(str(count) + '\t' + i +'\n')
             count = count + 1
 
+
 def get_author(fileName):
     authors_list = []
     with open(fileName,encoding='utf-8') as data_file:
@@ -283,4 +292,97 @@ def get_author(fileName):
         count = count +1
 
     output.close()
+
+
+def get_institutes(fileName):
+    """Extract insititutes information from original data, Output insititutes.txt """
+    institutes_list=[]
+    with open(fileName,encoding='utf-8') as data_file:
+        for line in data_file:
+            data = json.loads(line)
+            if "institutes" in data:
+                #print(data["institutes"])
+                institutes_list = institutes_list + data["institutes"]
+
+    institutes_list = sorted(set(institutes_list))
+    output = open("insititutes.txt", 'wt', encoding='utf-8')
+    count = 1
+    for institute in institutes_list:
+        print(count, institute)
+        content = institute.split(',')
+        output.write(str(count) )
+        for item in content:
+            output.write('\t'+ item.replace('"','' ))
+        output.write('\n')
+
+        count = count + 1
+
+    output.close()
+
+
+def get_proceeding(fileName):
+    """Get proceeding information from original data, Some proceeding information is missing.
+    Output proceeding.txt file, including the conference abbreviation and the year"""
+    with open(fileName, encoding='utf-8') as data_file:
+        proceeding_list = []
+        pattern = re.compile(r'.*Proceedings') # match proceeding information
+        for line in data_file:
+            data = json.loads(line)
+
+            if "proceeding" in data:
+                proceeding = data["proceeding"].replace('\n',' ')
+                m = pattern.match(proceeding)
+                if m is not None: # matched
+                    content = m.group().replace('Proceedings',' ')
+                    proceeding_list.append(content)
+
+
+
+        proceeding_list = sorted(set(proceeding_list))
+        #print(proceeding_list)
+        count = 1
+        output = open("proceeding.txt", 'wt', encoding='utf-8')
+        p = re.compile(r'[0-9]+')
+        for proceeding in proceeding_list:
+            print(proceeding)
+            proceeding = proceeding.replace('\'',' ' )
+            pro_items = proceeding.split(' ')  # get conference information
+            output.write(str(count) + '\t'+ pro_items[0] + '\t')
+            count = count + 1
+
+            a = p.search(proceeding).group()
+            year = int( a) # get year
+            if year < 20: # 2000s
+                year = 2000+year
+            else: # 1900s
+                year = 1900+year
+            output.write(str(year) + '\n')
+
+
+def get_conference(fileName):
+    """ Get all "conference inforamtion from original data.
+    Output is conference.txt file contains all conference and their abbreviation."""
+    conference_list = []
+    count = 0
+    with open(fileName,encoding='utf-8') as data_file:
+        for line in data_file:
+            data = json.loads(line)
+            count = count + 1
+            if "publish" in data:
+                print(count,data["publish"],'          ',data["title"])
+                conference_list.append(data["publish"].strip())
+
+    conferences = sorted(set(conference_list))
+
+    output = open("conference.txt", 'wt', encoding='utf-8')
+    count = 1
+    for conference in conferences:
+        print(count, conference)
+        # get all uppercase letters as the abbreviation
+        abbreviation = ''.join([char for char in conference if char.isupper()] )
+        output.write(str(count) + '\t' + abbreviation +'\t'+ conference + '\n')
+        count = count + 1
+
+    output.close()
+
 
